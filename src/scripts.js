b6903957c4cfeaea895a1ec164bfc8e07f2d9b12
ailsonguedes17 <contato@ailsonguedes.com.br>
2023-04-10 17:21:01 -0300

    primeiro-commit

---
+++ src/scripts.js
@@ -0,0 +1,10 @@
+
+function trocaCor(cor) {
+    let circulo = document.querySelector(".circulo")
+    circulo.style.background = cor
+}
+
+function trocaImagen(imagem) {
+    let imgIphone = document.querySelector(".iphone")
+    imgIphone.src = imagem
+}
